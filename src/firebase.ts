ed6d3f3241569291d71036b8c291f99c671ed5fb
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-05-19 21:07:36 +0900

    May-19

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,1 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDBbVmHSGnbOIV9biBjv9wY--owtwj97yQ",
+  authDomain: "twitter-app-cbc42.firebaseapp.com",
+  projectId: "twitter-app-cbc42",
+  storageBucket: "twitter-app-cbc42.firebasestorage.app",
+  messagingSenderId: "881732125564",
+  appId: "1:881732125564:web:e4f70faaa2b0f9ace31af9",
+  measurementId: "G-082Y4LE0QH"
+};
