99a50b85e8ff912a79e781cbe817a24813e0b4a3
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-05-20 16:24:49 +0900

    May-20

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
+import React, { useState } from 'react';
+import styles from "./Auth.module.css";
+import { useDispatch } from "react-redux";
+import { auth, provider, storage } from "../firebase";
+
+import {
+    Avatar,
+    Button,
+    CssBaseline,
+    TextField,
+    FormControlLabel,
+    Checkbox,
+    Link,
+    Paper,
+    Box,
+    Grid,
+    Typography,
+    makeStyles
+} from "@material-ui/core";
+
+import SendIcon from "@material-ui/icons/Send";
+import CameraIcon from "@material-ui/icons/Camera";
+import EmailIcon from "@material-ui/icons/Email";
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 function Copyright() {
     return (
