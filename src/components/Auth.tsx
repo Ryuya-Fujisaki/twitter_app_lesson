93d72f9aa2233cb7bea92babfbd46408497ae7db
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-05-20 18:02:50 +0900

    May-20

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -57,6 +57,17 @@ const useStyles = makeStyles((theme) => ({
 
 const Auth: React.FC = () => {
     const classes = useStyles();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLogin, setIsLogin] = useState(true);
+
+    const signInEmail = async () => {
+        await auth.signInWithEmailAndPassword(email, password);
+    };
+    const signUpEmail = async () => {
+        await auth.createUserWithEmailAndPassword(email, password);
+    };
+
     const signInGoogle = async () => {
         await auth.signInWithPopup(provider).catch((err) => alert(err.message));
     };
@@ -70,7 +81,7 @@ const Auth: React.FC = () => {
                         <LockOutlinedIcon />
                     </Avatar>
                     <Typography component="h1" variant="h5">
-                        Sign in
+                        {isLogin ? "Login" : "Register"}
                     </Typography>
                     <form className={classes.form} noValidate>
                         <TextField
@@ -83,6 +94,10 @@ const Auth: React.FC = () => {
                             name="email"
                             autoComplete="email"
                             autoFocus
+                            value={email}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                setEmail(e.target.value);
+                            }}
                         />
                         <TextField
                             variant="outlined"
@@ -94,17 +109,32 @@ const Auth: React.FC = () => {
                             type="password"
                             id="password"
                             autoComplete="current-password"
+                            value={password}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                setPassword(e.target.value);
+                            }}
                         />
-                        <Button
+                        < Button
                             type="submit"
                             fullWidth
                             variant="contained"
                             color="primary"
                             className={classes.submit}
                         >
-                            Sign In
+                            {isLogin ? "Login" : "Register"}
                         </Button>
-
+                        <Grid container>
+                            <Grid item xs>
+                                <span>Forgot password?</span>
+                            </Grid>
+                            <Grid item xs>
+                                <span
+                                    className={styles.login_toggleMode}
+                                    onClick={() => setIsLogin(!isLogin)}
+                                >
+                                    {!isLogin ? "Create new account ?" : "Back to login"}</span>
+                            </Grid>
+                        </Grid>
                         <Button
                             fullWidth
                             variant="contained"
@@ -118,7 +148,7 @@ const Auth: React.FC = () => {
                     </form>
                 </div>
             </Grid>
-        </Grid>
+        </Grid >
     );
 };
 export default Auth;
