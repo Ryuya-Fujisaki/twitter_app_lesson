f6984a2feef3c25b240164153105d9252d793133
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-05-20 12:16:36 +0900

    May-20

--- src/features/userSlice.ts
+++ src/features/userSlice.ts
@@ -9,21 +9,18 @@ export const userSlice = createSlice({
   },
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    increment: (state) => {
-      state.value += 1;
+    login: (state, action) => {
+      state.user = action.payload;
     },
-    decrement: (state) => {
-      state.value -= 1;
+    logout: (state) => {
+      state.user ={uid:"", photoUrl:"", displayName:""};
     },
     },
   },
-});
+);
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { login, logout } = userSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state: RootState) => state.counter.value;
+export const selectUser = (state: RootState) => state.user.user;
 
-export default counterSlice.reducer;
+export default userSlice.reducer;
