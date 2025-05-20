9ff5e192c34868595ba71a0f1f7909fbea9fbf11
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-05-20 11:55:42 +0900

    May-20

--- src/features/userSlice.ts
+++ src/features/userSlice.ts
@@ -19,9 +19,6 @@ export const userSlice = createSlice({
     decrement: (state) => {
       state.value -= 1;
     },
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload;
     },
   },
 });
