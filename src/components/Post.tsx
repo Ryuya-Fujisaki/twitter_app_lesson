70791511b47d823538b1e097b8b5c94a7a652e9c
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-05-22 16:48:14 +0900

    May-22

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -20,6 +20,19 @@ interface PROPS {
 }
 
 const Post: React.FC<PROPS> = (props) => {
+    const user = useSelector(selectUser);
+    const [comment, setComment] = useState("");
+    const newComment = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        db.collection("posts").doc(props.postId).collection("comments").add({
+            avatar: user.photoUrl,
+            text: comment,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            username: user.displayName
+        });
+        setComment("");
+    };
+
     return (
         <div className={styles.post}>
             <div className={styles.post_avatar}>
@@ -44,6 +57,11 @@ const Post: React.FC<PROPS> = (props) => {
                         <img src={props.image} alt="tweet" />
                     </div>
                 )}
+                <form onSubmit={newComment}>
+                    <div className={styles.post_form}>
+                        <input type="text" />
+                    </div>
+                </form>
             </div>
         </div>
     )
